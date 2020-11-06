b0892e5dc2fae9512beb6f2b8f5d656decc51363
Gunil <gunil.na1@gmail.com>
2020-11-07 00:28:43 +0100

    added sample data

---
+++ backend/data/users.js
@@ -0,0 +1,20 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Gunil Na",
+    email: "gunil@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Melanie Barrionuevo",
+    email: "melanie@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
