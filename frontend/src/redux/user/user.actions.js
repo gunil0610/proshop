0e93585e566a176b688600d0e923505e7591a316
Gunil <gunil.na1@gmail.com>
2020-11-12 16:17:28 +0100

    show user in navbar and logout

--- frontend/src/redux/user/user.actions.js
+++ frontend/src/redux/user/user.actions.js
@@ -36,3 +36,8 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 };
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({ type: UserActionTypes.USER_LOGOUT });
+};
