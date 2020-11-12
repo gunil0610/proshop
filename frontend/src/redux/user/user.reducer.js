b67c90a522ee3e483401132a66c8d47cba964475
Gunil <gunil.na1@gmail.com>
2020-11-12 17:18:36 +0100

    profile screen and get user details

--- frontend/src/redux/user/user.reducer.js
+++ frontend/src/redux/user/user.reducer.js
@@ -6,17 +6,22 @@ const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case UserActionType.USER_LOGIN_REQUEST:
     case UserActionType.USER_REGISTER_REQUEST:
+    case UserActionType.USER_DETAILS_REQUEST:
       return {
+        ...state,
         loading: true,
       };
     case UserActionType.USER_LOGIN_SUCCESS:
     case UserActionType.USER_REGISTER_SUCCESS:
+    case UserActionType.USER_DETAILS_SUCCESS:
       return {
         loading: false,
         userInfo: action.payload,
       };
+
     case UserActionType.USER_LOGIN_FAIL:
     case UserActionType.USER_REGISTER_FAIL:
+    case UserActionType.USER_DETAILS_FAIL:
       return {
         loading: false,
         error: action.payload,
