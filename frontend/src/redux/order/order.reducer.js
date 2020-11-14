5a99a49004c2d2d7ffcb48e949c2a01bb82c663f
Gunil <gunil.na1@gmail.com>
2020-11-14 23:22:19 +0100

    removing useless codes

--- frontend/src/redux/order/order.reducer.js
+++ frontend/src/redux/order/order.reducer.js
@@ -7,14 +7,10 @@ const INITIAL_STATE = {
 const orderReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case OrderActionTypes.ORDER_CREATE_REQUEST:
-    case OrderActionTypes.ORDER_DETAILS_REQUEST:
       return { ...state, loading: true };
     case OrderActionTypes.ORDER_CREATE_SUCCESS:
       return { loading: false, success: true, order: action.payload };
-    case OrderActionTypes.ORDER_DETAILS_SUCCESS:
-      return { loading: false, order: action.payload };
     case OrderActionTypes.ORDER_CREATE_FAIL:
-    case OrderActionTypes.ORDER_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
